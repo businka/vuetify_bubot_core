2df012e81d7df2f13f05ac1fb7bab03854b02a7c
businka <1338833@gmail.com>
2023-01-24 17:01:50 +0300

    websocket.js add appname

--- plugins/websocket.js
+++ plugins/websocket.js
@@ -1,8 +1,9 @@
 import VueNativeSock from 'vue-native-websocket'
 import Vue from 'vue'
+import AppConst from '../../AppConst'
 // import store from '../../store'
 
-Vue.use(VueNativeSock, `ws://${window.location.host}/ws`, {
+Vue.use(VueNativeSock, `ws://${window.location.host}/${AppConst.appName}/ws`, {
   reconnection: true, // (Boolean) whether to reconnect automatically (false)
   // reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
   reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
