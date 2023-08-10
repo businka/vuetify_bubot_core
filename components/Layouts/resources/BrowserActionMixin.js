9d3699034b3c982244c01c8e04f78ae66ef30a3a
businka <1338833@gmail.com>
2023-08-10 18:39:16 +0300

    @

--- components/Layouts/resources/BrowserActionMixin.js
+++ components/Layouts/resources/BrowserActionMixin.js
@@ -1,5 +1,5 @@
 import {initDataSource} from '../../DataSource/DataSourceLoader'
-import {updateObject, objHasOwnProperty} from '../../../../Helpers/BaseHelper'
+import {updateObject, objHasOwnProperty} from '@/Helpers/BaseHelper'
 // import Vue from 'vue'
 
 export default {
